9fae70c41d9c4aaa4d864ccc8341f62aed6b425e
AdrienPVM <adrien.pvm@gmail.com>
2017-10-30 17:12:25 +0100

    Correction boutyon delete qui fonctionne désormais

--- public/edit.php
+++ public/edit.php
@@ -1,23 +1,21 @@
-<pre>
-
-    <?php
+<?php
     require __DIR__.'/../inc/config.php';
 
     if (isset($_POST['delete'])) {
         # code...
     }
-    $sqlDelete='DELETE *
-    FROM student
+    $sqlDelete='DELETE FROM student
     WHERE stu_id = :deleteID
     ';
     $pdoDelete=$pdo->prepare($sqlDelete);
     $pdoDelete->bindValue(':deleteID', $_POST['id'], PDO::PARAM_INT);
-    $pdoDelete->execute();
 
-    if ($pdoDelete===false) {
+    if ($pdoDelete->execute()===false) {
         print_r($pdoDelete->errorInfo(), true);
     }
+    echo $pdoDelete->rowCount();
+
 
     header('Location: http://projet-toto.dev/list.php');
-    ?>
-</pre>
+    exit; //pour éviter tout problème
+?>
